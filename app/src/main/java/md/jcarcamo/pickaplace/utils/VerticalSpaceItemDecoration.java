0110a200f5d82392aeac221e003a8d3d4eea8647
Juan Gonzalo Cárcamo <jcarcamo@gmail.com>
2017-06-20 14:05:34 -0400

    Added a little style. Version to present today

---
+++ app/src/main/java/md/jcarcamo/pickaplace/utils/VerticalSpaceItemDecoration.java
@@ -0,0 +1,10 @@
+package md.jcarcamo.pickaplace.utils;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by juan_ on 6/20/2017.
+ */
+
+public class VerticalSpaceItemDecoration extends RecyclerView.ItemDecoration {
+}
